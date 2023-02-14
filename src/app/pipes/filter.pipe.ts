6fc6ebccbdc8f18f32660b96d3ba8ab88e0a93c3
volodymyr22 <volodymyr.zhyhaylo@gmail.com>
2023-02-14 15:29:56 +0200

    rewrite filter pipe

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -9,15 +9,13 @@ export class FilterPipe implements PipeTransform {
   transform(items: Question[], value: string): Question[] {
     if (!items) return [];
     if(value) {
-      return items.filter(({title, english, ukrainian}) => {
-        return(
-          title.toLocaleLowerCase().indexOf(value.toLocaleLowerCase()) > -1
-          ||
-          english?.toLocaleLowerCase().indexOf(value.toLocaleLowerCase()) > -1
-          ||
-          ukrainian?.toLocaleLowerCase().indexOf(value.toLocaleLowerCase()) > -1
-        )
-      });
+      let filteredTitles = items.filter(({title}) => title.toLocaleLowerCase().indexOf(value.toLocaleLowerCase()) > -1)
+      let filteredEnglish = items.filter(({english}) => english.toLocaleLowerCase().indexOf(value.toLocaleLowerCase()) > -1)
+      let filteredUkrainian  = items.filter(({ukrainian}) => ukrainian.toLocaleLowerCase().indexOf(value.toLocaleLowerCase()) > -1)
+
+      return [...filteredTitles, ...filteredEnglish, ...filteredUkrainian]
+             .filter((value, index, self) => self.indexOf(value) === index)
+
 
     }
     else {
